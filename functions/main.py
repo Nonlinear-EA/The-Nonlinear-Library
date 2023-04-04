e9c5a2bbb1006ce99ffee6c4d92789bc3f619c35
Max Pietsch <maximumpeaches@users.noreply.github.com>
2023-04-03 20:49:30 -0500

    Remove function. in imports b/c GCP can't recognize it.

--- functions/main.py
+++ functions/main.py
@@ -1,19 +1,19 @@
-
-from functions.af.alignment_forum import af_all_main
-from functions.af.alignment_forum_daily import af_daily_main
-from functions.af.alignment_forum_weekly import af_weekly_main
-from functions.ea.effective_altruism_forum import ea_all_main
-from functions.ea.effective_altruism_forum_daily import ea_daily_main
-from functions.ea.effective_altruism_forum_weekly import ea_weekly_main
-from functions.lw.lesswrong import lw_all_main
-from functions.lw.lesswrong_daily import lw_daily_main
-from functions.lw.lesswrong_weekly import lw_weekly_main
-from functions.nnl import main_create_beyondwords_nonlinear_library_project_inputs
+from af.alignment_forum import af_all_main
+from af.alignment_forum_daily import af_daily_main
+from af.alignment_forum_weekly import af_weekly_main
+from ea.effective_altruism_forum import ea_all_main
+from ea.effective_altruism_forum_daily import ea_daily_main
+from ea.effective_altruism_forum_weekly import ea_weekly_main
+from lw.lesswrong import lw_all_main
+from lw.lesswrong_daily import lw_daily_main
+from lw.lesswrong_weekly import lw_weekly_main
+from nnl import main_create_beyondwords_nonlinear_library_project_inputs
 
 
 def af_daily(a=None, b=None):
     print('running af_daily')
     af_daily_main(True)
+
 
 def af_weekly(a=None, b=None):
     print('running af_weekly')
