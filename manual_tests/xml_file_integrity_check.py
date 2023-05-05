8be031092b859b0d950edfc13f9e5e7c96ee5e7e
Andrés Arango Pérez <a.arango@tu-braunschweig.de>
2023-05-05 14:51:02 -0500

    Merge xml integrity checks

--- manual_tests/xml_file_integrity_check.py
+++ manual_tests/xml_file_integrity_check.py
@@ -1,13 +1,20 @@
-import logging
-import os.path
-
+from google.cloud import logging
 from lxml import etree
 from lxml.etree import XMLParser, XMLSyntaxError
 
 from feed_processing.feed_updaters import download_file_from_url
 
 xml_files_urls = [
-    "https://storage.googleapis.com/rssfile/nonlinear-library-aggregated-EA.xml"
+    "https://storage.googleapis.com/rssfile/nonlinear-library-aggregated-EA.xml",
+    "https://storage.googleapis.com/rssfile/nonlinear-library-aggregated-EA-daily.xml",
+    "https://storage.googleapis.com/rssfile/nonlinear-library-aggregated-EA-weekly.xml",
+    "https://storage.googleapis.com/rssfile/nonlinear-library-aggregated-AF.xml",
+    "https://storage.googleapis.com/rssfile/nonlinear-library-aggregated-AF-daily.xml",
+    "https://storage.googleapis.com/rssfile/nonlinear-library-aggregated-AF-weekly.xml",
+    "https://storage.googleapis.com/rssfile/nonlinear-library-aggregated-LW.xml",
+    "https://storage.googleapis.com/rssfile/nonlinear-library-aggregated-LW-daily.xml",
+    "https://storage.googleapis.com/rssfile/nonlinear-library-aggregated-LW-weekly.xml",
+    "https://storage.googleapis.com/rssfile/nonlinear-library-aggregated.xml"
 ]
 
 
@@ -22,18 +29,21 @@ def xml_string_is_valid(xml_str):
     return None
 
 
-if __name__ == '__main__':
-    logger = logging.getLogger("xml_file_integrity_check")
-    logger.setLevel(logging.INFO)
+def check_xml_files_integrity(urls, running_on_gcp=True):
+    logging_client = logging.Client()
 
-    for xml_file_url in xml_files_urls:
+    # This log can be found in the Cloud Logging console under 'Custom Logs'.
+    logger = logging_client.logger("XML_Integrity_Checks")
+
+    for xml_file_url in urls:
         xml_str = download_file_from_url(xml_file_url, cache=False)
         xml_file_validation_exception = xml_string_is_valid(xml_str)
         if xml_file_validation_exception:
-            logger.critical(f"File '{xml_file_url}' is invalid. Exception: {xml_file_validation_exception}")
-
-            # Save file for inspection
-            with open(os.path.join("./integrity_check_files", os.path.basename(xml_file_url)), "rb") as f:
-                f.write(xml_str)
+            logger.log_text(f"File '{xml_file_url}' is invalid. Exception: {xml_file_validation_exception}",
+                            severity="CRITICAL")
         else:
-            logger.log(logging.INFO, "File '{xml_file_url}' okay.")
+            logger.log_text(f"File '{xml_file_url}' okay.")
+
+
+if __name__ == '__main__':
+    check_xml_files_integrity(xml_files_urls)
